358efa8ff94c80785f90bd598023e70d0ca55614
Frederic Pillon <frederic.pillon@st.com>
2020-05-25 14:50:52 +0200

    Rework HelloWorld sketch for target with limited RAM
    Arduino UNO does not have enough memory to run properly the sketch
    using Serial and read/writeURI.
    
    To be comptabile, LED status replaces the Serial monitor usage.
    
    Fixes #10
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- examples/X_NUCLEO_NFC04A1_HelloWorld/X_NUCLEO_NFC04A1_HelloWorld.ino
+++ examples/X_NUCLEO_NFC04A1_HelloWorld/X_NUCLEO_NFC04A1_HelloWorld.ino
@@ -46,9 +46,11 @@
   It writes an NFC tag type URI (Uniform Resource Identifier) and reads this same tag.
   Choose the uri by changing the content of uri_write.
 
-  When the NFC module is started and ready, the message "Sytstem init done!" is
-  displayed on the monitor window. Next, the tag is written, read and printed on
-  the monitor window.
+  When the NFC module is started and ready, the green LED switches ON else it blinks.
+  Next, the tag is written, the blue LED switches ON else it blinks.
+  Then read is performed, the yellow LED switches ON else it blinks.
+  Finally, the written and read value are compared, if they does not match then
+  all LED's blink.
 
   You can also use your smartphone to read/write a tag.
   On Android, donwload a NFC Tools. Then start the app, check if NFC is activated
@@ -59,28 +61,29 @@
 
 #include "x_nucleo_nfc04.h"
 
-#define SerialPort      Serial
-
 void setup() {
   const char uri_write_message[] = "st.com/st25";       // Uri message to write in the tag
   const char uri_write_protocol[] = URI_ID_0x01_STRING; // Uri protocol to write in the tag
   String uri_write = String(uri_write_protocol) + String(uri_write_message);
   String uri_read;
 
-  // Initialize serial for output.
-  SerialPort.begin(115200);
-
-  if (X_Nucleo_Nfc04.begin() == 0) {
-    SerialPort.println("System Init done!");
-    X_Nucleo_Nfc04.ledOn(GREEN_LED);
-  } else {
-    SerialPort.println("System Init failed!");
-    while (1);
+  if (X_Nucleo_Nfc04.begin()) {
+    while (1) {
+      X_Nucleo_Nfc04.ledOn(GREEN_LED);
+      delay(100);
+      X_Nucleo_Nfc04.ledOff(GREEN_LED);
+      delay(100);
+    }
   }
+  X_Nucleo_Nfc04.ledOn(GREEN_LED);
 
   if (X_Nucleo_Nfc04.writeURI(uri_write_protocol, uri_write_message, "")) {
-    SerialPort.println("Write failed!");
-    while (1);
+    while (1) {
+      X_Nucleo_Nfc04.ledOn(BLUE_LED);
+      delay(100);
+      X_Nucleo_Nfc04.ledOff(BLUE_LED);
+      delay(100);
+    }
   }
 
   X_Nucleo_Nfc04.ledOn(BLUE_LED);
@@ -88,17 +91,27 @@ void setup() {
   delay(100);
 
   if (X_Nucleo_Nfc04.readURI(&uri_read)) {
-    SerialPort.println("Read failed!");
-    while (1);
+    while (1) {
+      X_Nucleo_Nfc04.ledOn(YELLOW_LED);
+      delay(100);
+      X_Nucleo_Nfc04.ledOff(YELLOW_LED);
+      delay(100);
+    }
   }
 
-  SerialPort.println(uri_read.c_str());
+  X_Nucleo_Nfc04.ledOn(YELLOW_LED);
 
-  if (strcmp(uri_read.c_str(), uri_write.c_str()) == 0) {
-    SerialPort.println("Successfully written and read!");
-    X_Nucleo_Nfc04.ledOn(YELLOW_LED);
-  } else {
-    SerialPort.println("Read bad string!");
+  if (strcmp(uri_read.c_str(), uri_write.c_str()) != 0) {
+    while (1) {
+      X_Nucleo_Nfc04.ledOn(GREEN_LED);
+      X_Nucleo_Nfc04.ledOn(BLUE_LED);
+      X_Nucleo_Nfc04.ledOn(YELLOW_LED);
+      delay(100);
+      X_Nucleo_Nfc04.ledOff(GREEN_LED);
+      X_Nucleo_Nfc04.ledOff(BLUE_LED);
+      X_Nucleo_Nfc04.ledOff(YELLOW_LED);
+      delay(100);
+    }
   }
 }
 
